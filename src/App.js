ee0ddf7df1fd7df21a48b80bfbf041965f1db1ff
unknown <rochellecompendio2021@u.northwestern.edu>
2020-04-21 11:36:37 -0500

    slice 4

--- src/App.js
+++ src/App.js
@@ -1,40 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import 'rbx/index.css';
 import { Button, Container, Title } from 'rbx';
 
-const schedule = {
-  "title": "CS Courses for 2018-2019",
-  "courses": [
-    {
-      "id": "F101",
-      "title": "Computer Science: Concepts, Philosophy, and Connections",
-      "meets": "MWF 11:00-11:50"
-    },
-    {
-      "id": "F110",
-      "title": "Intro Programming for non-majors",
-      "meets": "MWF 10:00-10:50"
-    },
-    {
-      "id": "F111",
-      "title": "Fundamentals of Computer Programming I",
-      "meets": "MWF 13:00-13:50"
-    },
-    {
-      "id": "W111",
-      "title": "Fundamentals of Computer Programming I",
-      "meets": "MWF 11:00-11:50"
-    },
-    {
-      "id": "F211",
-      "title": "Fundamentals of Computer Programming II",
-      "meets": "TuTh 12:30-13:50"
-    }
-  ]
-};
-
 const terms = { F: 'Fall', W: 'Winter', S: 'Spring'};
-
 
 const Banner = ({ title }) => (
   <h1 className="title">{ title }</h1>
@@ -60,11 +28,29 @@ const CourseList = ({ courses }) => (
   </div>
 );
 
-const App = () =>  (
-  <div className="container">
-    <Banner title={ schedule.title } />
-    <CourseList courses={ schedule.courses } />
-  </div>
-);
+const App = () => {
+  const [schedule, setSchedule] = React.useState({ title: '', courses: [] });
+  const url = 'https://courses.cs.northwestern.edu/394/data/cs-courses.php';
+
+  React.useEffect(() => {
+    
+    const fetchSchedule = async () => {
+      const response = await fetch(url);
+      if (!response.ok) throw response;
+      const json = await response.json();
+      setSchedule(json);
+    }
+    
+    fetchSchedule();
+  }, []);
+
+  return (
+    <div className="container">
+      <Banner title={ schedule.title } />
+      <CourseList courses={ schedule.courses } />
+    </div>
+  );
+};
+
 
 export default App;
